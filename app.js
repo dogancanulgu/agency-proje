c3ebd35d7f7d799a32fa3aeb729233a4c17dcafe
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-12-01 23:58:22 +0300

    setup ejs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
 // middlewares
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(port, () => {
